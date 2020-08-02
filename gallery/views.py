7e99b02786e6d1294d1c2867c58eba69f07bf9d5
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-02 13:57:21 +0300

    Add dinamic content

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime as dt
 
 # Create your views here.
@@ -16,7 +16,7 @@ def gallery_of_day(request):
             </body>
         </html>
             '''
-    return HttpResponse(html)
+    return render(request, 'gallery/today-gallery.html', {"date":date})
 
 def convert_dates(dates):
     
@@ -28,16 +28,19 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
-def past_days_gallery(request,past_date):
-    # Converts data from the string Url
-    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+def past_days_gallery(request, past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(gallery_of_day)
+
+    return render(request, 'gallery/past-gallery.html', {"date": date})
